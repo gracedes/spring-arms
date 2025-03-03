5c38a2be9c72066c2a848aeb0ca547241044fcb4
Grace <gracedes@bu.edu>
2025-03-03 18:16:09 -0500

    fixed

--- entities/player/arm/arm.gd
+++ entities/player/arm/arm.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var grab = false
+var gpos = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,4 +9,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if grab == true:
+		self.get_node('hand').set_global_position(gpos)
+		if Input.is_action_just_pressed('leftClick'):
+			grab = false
+	else:
+		if Input.is_action_just_pressed('leftClick'):
+			grab = true
+			gpos = get_viewport().get_mouse_position()
