01b671796c76d4681c38704df088ccca1b512f9e
grace <gracedes@bu.edu>
2025-04-10 17:34:49 -0400

    arm forces implemented

--- entities/player/arm/arm.gd
+++ entities/player/arm/arm.gd
@@ -1,19 +1,13 @@
 extends Node2D
 
+@export var key: String
 var grab = false
 var gpos = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.get_node('spring').node_b = '../..'
+	get_node('hand').visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if grab == true:
-		self.get_node('hand').set_global_position(gpos)
-		if Input.is_action_just_pressed('leftClick'):
-			grab = false
-	else:
-		if Input.is_action_just_pressed('leftClick'):
-			grab = true
-			gpos = get_viewport().get_mouse_position()
+	get_node('hand').look_at(get_parent().global_position)
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,12 +1,17 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -1000.0
+var arms = []
+@export var max_dist = 10
+@export var spring_coeff = 5
 
+func _ready() -> void:
+	arms = get_tree().get_nodes_in_group('arms')
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	"""# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -22,4 +27,34 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	move_and_slide()"""
+	check_inputs()
+	apply_force(sum_arm_forces())
+
+func check_inputs() -> void:
+	for arm in arms:
+		if arm.grab == true:
+			arm.get_node('hand').set_global_position(arm.gpos)
+			# arm.get_node('hand').set_global_position(global_position + (max_dist*Vector2.RIGHT).rotated(global_position.angle_to_point(arm.gpos)))
+			if Input.is_action_just_pressed(arm.key):
+				arm.grab = false
+				arm.get_node('hand').visible = false
+		else:
+			if Input.is_action_just_pressed(arm.key):
+				arm.gpos = get_viewport().get_mouse_position()
+				arm.grab = true
+				arm.get_node('hand').visible = true
+
+func sum_arm_forces() -> Vector2:
+	var f = Vector2.ZERO
+	for arm in arms:
+		if arm.get_node('hand').visible:
+			var agpos = arm.get_node('hand').global_position
+			var sgpos = global_position
+			
+			var dist = sgpos.distance_to(agpos)
+			var angle = sgpos.angle_to_point(agpos)
+			
+			f += spring_coeff*(agpos-sgpos)
+	print(f)
+	return f
