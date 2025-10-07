c9b5315c3049a8d3c77aa59f28a3acd10a9fa446
grace <gracedes@bu.edu>
2025-10-07 13:04:54 -0400

    added hair physics, vars still need tuning

---
+++ entities/player/hair/hair.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+@export var max_shift = 30.0
+@export var shift_coeff = 0.001
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var player = get_parent().get_parent()
+	move_to(player.linear_velocity.y)
+
+func move_to(yvelo: float) -> void:
+	if abs(yvelo) <= 0.5:
+		offset.y = move_toward(offset.y, -54.0, 500 * shift_coeff)
+	else:
+		print(yvelo * shift_coeff)
+		offset.y = move_toward(offset.y, -54.0 + (max_shift * -(yvelo / abs(yvelo))), min(abs(yvelo) * shift_coeff, 500 * shift_coeff))
+	pass
